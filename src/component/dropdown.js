f6fb78a263201a46e54ffa1b4b9bdc6120386864
Cj_dozie <ibennejohn064@gmail.com>
2023-09-16 03:50:00 -0700

    second commit

--- src/component/dropdown.js
+++ src/component/dropdown.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useState,useEffect } from "react";
-import Nigeria from "./NIgeria";
 
 const Drop = () => {
     const[ Data, setData]= useState('');
@@ -17,80 +16,20 @@ const Drop = () => {
     },[])
 
 
-    const NG = () =>{
-        fetch(`https://sba-fkqb.onrender.com/NG`, {method:'GET'})
-        .then((res)=> res.json())
-        .then((data) => {
-            setData(data)
-            console.log(data);
-        })
-    }
-
-    const Ug = () =>{
-        fetch(`https://sba-fkqb.onrender.com/ug`, {method:'GET'})
-        .then((res)=> res.json())
-        .then((data) => {
-            setData(data)
-            console.log(data);
-        })
-    }
-
-    const ZW = () =>{
-        fetch(`https://sba-fkqb.onrender.com/zw`, {method:'GET'})
-        .then((res)=> res.json())
-        .then((data) => {
-            setData(data)
-            console.log(data);
-        })
-    }
-
-    const IC = () =>{
-        fetch(`https://sba-fkqb.onrender.com/IC`, {method:'GET'})
-        .then((res)=> res.json())
-        .then((data) => {
-            setData(data)
-            console.log(data);
-        })
-    }
-
-    const TZ = () =>{
-        fetch(`https://sba-fkqb.onrender.com/TZ`, {method:'GET'})
-        .then((res)=> res.json())
-        .then((data) => {
-            setData(data)
-            console.log(data);
-        })
-    }
-
-    const EG = () =>{
-        fetch(`https://sba-fkqb.onrender.com/EG`, {method:'GET'})
-        .then((res)=> res.json())
-        .then((data) => {
-            setData(data)
-            console.log(data);
-        })
-    }
-
-    const MW = () =>{
-        fetch(`https://sba-fkqb.onrender.com/MW`, {method:'GET'})
-        .then((res)=> res.json())
-        .then((data) => {
-            setData(data)
-            console.log(data);
-        })
-    }
 
     const Views = (data) => {
         if (data) {
             return data.map((item)=>{
                 return(
-                   <table>
-                    <thead key={0}>
-                        <tr>
-                            <th>{item[0][0]}</th>
-                        </tr>
-                    </thead>
-                   </table>
+                    <>
+                        <tbody>
+                            <tr>
+                                <td>{item[0]}</td>
+                                <td>{item[1]}</td>
+                                <td>{item[2]}</td>
+                            </tr>
+                        </tbody>
+                    </>
                 )
             })
         } else{
@@ -113,7 +52,16 @@ const Drop = () => {
             </select>
 
             <div>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                            <th>Title</th>
+                            <th>Date<br/> Published</th>
+                            <th></th>
+                    </tr>
+                </thead>
                 {Views(Data)}
+                </table>
             </div>
 
         </>
